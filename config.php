34e87488cff497a8a94c925d68fefa6a290e59b8
Amanguchi <johncorderox@gmail.com>
2017-01-20 00:50:56 -0800

    Adding a config PHP file

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+$name = "John";                                   //Username for the gobal system
+$websitename = "YourWebsite";                     //Name of website to
+
+
+
+
+// MySQL Configuration
+$servername = "localhost";                        //Server name of Host
+$username = "root";                               //MySQL Username.
+$password = "root";                               //MySQL Password. A password with a length < 8 characters is suggested.
+$database = "";
+$table = "bugs";
+
+
+ ?>
