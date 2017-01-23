02358586780f1ac4d5c9151721b1ff602e2a41f2
Amanguchi <johncorderox@gmail.com>
2017-01-23 00:48:50 -0800

    front end changes done with index.

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
   <html lang="en">
     <head>
       <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
       <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
       <script src="bower_components/jquery/dist/jquery.min.js"></script>
       <link href="src/css/interface.css" rel="stylesheet" />
     </head>
   <body>
-
-<div class="welcome">
+<div class="center">
    <h2>Welcome.</h2>
      <form action="index.php" method="POST">
      <input type="text" placeholder="Username *" name="username"/><br />
