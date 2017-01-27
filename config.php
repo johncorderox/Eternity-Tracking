e61f1777c1b64e18bbb4f6345222d78068e425fd
Amanguchi <johncorderox@gmail.com>
2017-01-27 00:45:17 -0800

    milestone for current development process. More HTML/css changes. switching from php extenions to html just to get the front end working with the browser refresh applcation.

--- config.php
+++ config.php
@@ -1,17 +1,16 @@
 <?php
 
 $name = "John";                                   //Username for the gobal system
-$websitename = "YourWebsite";                     //Name of website to
+$websitename = "YourWebsite.com";                 //Name of website to
 
 
 
 
 // MySQL Configuration
-$servername = "localhost";                        //Server name of Host
+$servername = "127.0.0.1";                        //Server name of Host
 $username = "root";                               //MySQL Username.
 $password = "root";                               //MySQL Password. A password with a length < 8 characters is suggested.
-$database = "";
-$table = "bugs";
+$database = "tracking";                           //Default database nane
 
 
  ?>
