06464289e4f9c8d6357d22daae3e8b0a5e0118f2
Amanguchi <johncorderox@gmail.com>
2017-01-30 10:40:21 -0800

    some cleaning up

--- connect.php
+++ connect.php
@@ -1,49 +1,5 @@
 <?php
 
-require 'config.php';
-
-// Open a new connection to the MySQL Server
-$connect = mysqli_connect('$servername', '$username', '$password', '$database');
-// Check connection of MySQL server
-  if ($connect->connect_error) {
-
-      die($connect->connect_error);
-
-
-
-  }
-// Loads if connection is successful.
-
-echo 'Successful connection to ' .$servername . '<br />';
-/* for later
-
-<?php
-  if(isset($_POST['submit'])) {
-
-    if (!$connect) {
-      die(mysqli_connect_error());
-    } else {
-
-
-        $title = $_POST['title'];
-        $message = $_POST['message'];
-
-        $q = "INSERT INTO bugs (title, message)
-  VALUES ($title,$message)";
-
-        if (mysqli_query($connect, $q)) {
-
-
-          $connect->close();
-        }
-    }
-
-
-
-  }
-
+echo phpversion();
 
 ?>
-
-*/
- ?>
