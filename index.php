db380248524d9d63a24ed5e8307bd6ed2565fb1f
Amanguchi <johncorderox@gmail.com>
2017-01-30 10:43:13 -0800

    Changing extensions of html to php files

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+      <script src="bower_components/jquery/dist/jquery.min.js"></script>
+      <link href="src/css/interface.css" rel="stylesheet" />
+    </head>
+  <body>
+<div class="center">
+   <h2>Welcome.</h2>
+     <form action="" method="">
+     <input type="text" placeholder="Username *" name="username" id="username"><br />
+     <input type="password" placeholder="Password *" name="password" id="password" /><br />
+     <button id="login-button">Login</button><br />
+   </form>
+</div>
+</body>
+</html>
