ebbe9c191715f941e03fb0658e09340bcd447b33
Amanguchi <johncorderox@gmail.com>
2017-02-06 21:50:21 -0800

    New plugin being added to help with errors. update functions, update dependencies

--- src/js/view.js
+++ src/js/view.js
@@ -1,26 +1,41 @@
 $(document).ready(function(){
 });
 
-
-
-// test function to be called.
-function test() {
-
-  $('.addform').hide('fast');
-
-}
-
 // @desc Case system that outputs data to the screen when a user submits a form.
 // Things like Success! Error! or Warning! are common outputs.
-// Thanks bootstrap for your alerts.
+// Using bootstrap-growl plugin for easier output.
 //
 // @args Triggers for output. var = num
-function display_input_message() {
-
+function display_input_message(n) {
   switch (n) {
     case 0:
+            $.bootstrapGrowl("Success! Thank you for helping us continue make our website grow.", {
+              ele: 'body',
+              type: 'Success',
+              align: 'right',
+              offset: {from: 'top', amount: 110},
+              width: 450,
+              delay: 4000,
+              allow_dismiss: true,
+              stackup_spacing: 10
+            });
+            //Hides form so it cannot be submitted more than once.
+            $('.addform, .requirements').hide(1000);
             break;
-          
+    case 1:
+            $.bootstrapGrowl("It looks like you missed some information! Please fill in all fields.", {
+                  ele: 'body',
+                  type: 'danger',
+                  align: 'right',
+                  offset: {from: 'top', amount: 110},
+                  width: 450,
+                  delay: 4000,
+                  allow_dismiss: false,
+                  stackup_spacing: 10
+                });
+                break;
+
+
     default:
 
   }
