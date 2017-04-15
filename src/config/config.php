7081461e62c01d4630d4892cac5b361a8c53f369
Amanguchi <johncorderox@gmail.com>
2017-04-15 00:53:12 -0700

    Logs are now done, css changes will come

--- src/config/config.php
+++ src/config/config.php
@@ -7,9 +7,6 @@ $database = 'tracking';
 
 
 $company_name = "Eternity LLC";
-$max_logs = 100; // max amount of logs displayed.
-
-
 $connect = new mysqli("$servername", "$username", "$password", "$database");
 
   if (!$connect) {
--- src/logs/logs.php
+++ src/logs/logs.php
@@ -10,55 +10,32 @@ include ('../config/config.php');
 </body>
 <?php
 
-    $log_counter = 0;
-    $logArr = array();
 
 
+$query_logs = "SELECT * from logs ORDER BY `action_id` DESC";
+$result = mysqli_query($connect, $query_logs);
+  while($rows=mysqli_fetch_assoc($result)) {
 
-    $query_logs = "SELECT * from logs";
-    $result = mysqli_query($connect, $query_logs);
+      if ($rows['action'] == 'A') {
+        $a = "added";
 
-      while($rows=mysqli_fetch_assoc($result)) {
+      echo $rows['log_user'] . ' added a new bug on '. $rows['date'] . '.<br>';
+      echo '<hr />';
 
+      } else if ($rows['action'] == 'D') {
 
-
-          if ($rows['action'] == 'A') {
-
-            $a = "added";
-            array_push($logArr, $a);
-
-          //  echo $rows['log_user'] . ' added a new bug on '. $rows['date'] . '.<br>';
-
-
-          } else if ($rows['action'] == 'D') {
-
-            $d = "deleted";
-            array_push($logArr, $d);
-
-        //    echo $rows['log_user'] . ' has deleted the bug ID '. $rows['action_value'] . '.<br>';
-          }
-
-         else if ($rows['action'] == 'AU') {
-
-        //  echo $rows['log_user'] . ' created the user ' . $rows['action_value']. '.<br>';
-
-          }
-
-          else if ($rows['action'] == 'RU') {
-
-      //    echo $rows['log_user'] . ' removed user ID '. $rows['action_value']. '.<br>';
-
-        }
-
-
+        echo $rows['log_user'] . ' has deleted the bug ID '. $rows['action_value'] . '.<br>';
+        echo '<hr />';
       }
-
-      for ($x = 0; $x < 2; $x++) {
-
-
-        echo $logArr[$x];
+     else if ($rows['action'] == 'AU') {
+      echo $rows['log_user'] . ' created the user ' . $rows['action_value']. '.<br>';
+      echo '<hr />';
       }
-
+      else if ($rows['action'] == 'RU') {
+     echo $rows['log_user'] . ' removed user ID '. $rows['action_value']. '.<br>';
+     echo '<hr />';
+    }
+  }
 
 
 ?>
