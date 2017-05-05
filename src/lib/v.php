22d709c1748ab587762fd0d912922b982472bb61
Amanguchi <johncorderox@gmail.com>
2017-05-05 11:53:57 -0700

    working on the save module

--- src/lib/v.php
+++ src/lib/v.php
@@ -1,14 +1,37 @@
 <?php
 
-if (isset($_GET['save'])) {
-
-      if (isset($_GET['category'])) {
-
-        $g = $_GET['category'];
-        echo $g;
-
-      }
+include "../config/config.php";
 
 
-    }
+if (isset($_POST['cancel'])) {
+
+  header("Location: ../modules/main.php");
+}
+
+
+if (isset($_POST['save'])) {
+
+    //  $title_2    = $_POST['title'];
+      $category = $_POST['category'];
+      $priority = $_POST['priority'];
+      $message  = $_POST['message'];
+
+
+      $sql = "UPDATE bugs SET `category` = '$category' WHERE id = '$bug_id'";
+
+      mysqli_query($connect, $sql);
+
+      header("Location: ../modules/main.php");
+
+
+
+
+}
+
+
+
+
+
+
+
  ?>
--- src/modules/view.php
+++ src/modules/view.php
@@ -40,13 +40,6 @@ $logged = $_SESSION['username'];
  }
 
 
-
-  if (isset($_POST['cancel'])) {
-
-    header("Location: main.php");
-  }
-
-
  ?>
  <head>
    <meta charset="UTF-8">
@@ -62,12 +55,12 @@ $logged = $_SESSION['username'];
   <div main="main_content">
     <div class="panel panel-default">
   <div class="panel-body">
-    <form action="../lib/v.php" method="GET">
+    <form action="../lib/v.php" method="POST">
       <div class="container-fluid">
      <p id="larger">Bug ID: <?php echo $bug_id; ?></p>
      <p>Reported By: <?php echo $reported; ?></p>
      <label for="title">Title:</label>
-     <input type="text" id="title" class="form-control" value="<?php echo $title;?>"/>
+     <input type="text" name="title" id="title" class="form-control" value="<?php echo $title; ?>"/>
      <label for="select_box_priority">Priority: </label>
      <select  name="priority" class="form-control" id="select_box">
        <option value="Low">Low</option>
@@ -90,9 +83,9 @@ $logged = $_SESSION['username'];
      </select><br />
           <label for="message">Message: </label>
           <p id="date_display"><b>Date Reported: </b> <i><?php echo $clean_date; ?></i></p>
-          <textarea class="form-control" id="message" rows="7"><?php echo $message; ?></textarea><br />
+          <textarea class="form-control" id="message" name="message" rows="7"><?php echo $message; ?></textarea><br />
           <div class="button-center">
-            <button type="submit" class="btn btn-primary" name="submit_view" id="save">Save  <span class="glyphicon glyphicon-check"></span></button>
+            <button type="submit" class="btn btn-primary" name="save" id="save">Save  <span class="glyphicon glyphicon-check"></span></button>
             <button type="submit" class="btn btn-primary" name="delete" id="delete"> Delete  <span class="glyphicon glyphicon-trash"></span></button>
             <button type="submit" class="btn btn-primary" name="cancel" id="cancel"> Cancel </button>
           </div>
