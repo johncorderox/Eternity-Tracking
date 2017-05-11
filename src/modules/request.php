5e811931ae3e73c93732d5c499081869fc10aae6
Amanguchi <johncorderox@gmail.com>
2017-05-11 00:48:40 -0700

    front end is basic but complete. the backend now submits to MySQL but with no conditions

--- src/modules/request.php
+++ src/modules/request.php
@@ -1,5 +1,7 @@
 <?php
 
+include ('../config/config.php');
+
 if (isset($_POST['cancel'])) {
 
   header("Location: ../index.php");
@@ -14,7 +16,8 @@ if(isset($_POST['send_request'])) {
   $email    = $_POST['email'];
   $password = $_POST['password'];
   $message  = $_POST['message'];
-
+  $ip       = $_SERVER['REMOTE_ADDR'];
+/*
     if (empty($first)) {
 
     }
@@ -29,11 +32,35 @@ if(isset($_POST['send_request'])) {
 
     }else if (empty($password)) {
 
-      
+
     }
+          else {
+*/
+            $sql_username = "SELECT username FROM users WHERE username = '$username'";
+            $sql_email = "SELECT email FROM users WHERE email = '$email'";
+
+            $result_user = mysqli_query($connect, $sql_username);
+            $result_email = mysqli_query($connect, $sql_email);
+
+              if(mysqli_num_rows($result_user) >= 1) {
+
+                echo "Username Taken";
+              } else if (mysqli_num_rows($result_email)) {
+
+                echo "email taken!";
+              }
 
 
+                  $sql_request = "INSERT INTO `requests` (first_name, last_name, username, password, email, message, date, ip)
+                  VALUES ('$first', '$last', '$username','$password','$email', '$message', NOW(), '$ip');
+                  ";
+                  $result_request = mysqli_query($connect, $sql_request) or die(mysqli_error($connect));
+                  if($result_request) {
 
+                      header("Location: ../index.php");
+                  }
+
+    //  }
 
 }
 
@@ -57,17 +84,16 @@ if(isset($_POST['send_request'])) {
   <hr />
 <div class="form-body">
 </div>
-<?php echo $error; ?><br />
+<div class="request-container">
   <p id="larger">
     All information will be recorded and sent to the team for review. Once accepted, you'll be able to access the software.
   </p>
   <p id="etc">
     Please use real information so the team can identify who you are and accept/deny the request.
   </p><br />
-<div class="request-container">
 <form action="request.php" method="POST">
-  <input type="text" id="small_input" placeholder="First Name*" name="first_name"/>
-  <input type="text" id="small_input" placeholder="Last Name*" name="last_name"/><br />
+  <input type="text" id="small_input" placeholder="First Name*" name="first_name" autofocus/>
+  <input type="text" id="small_input2" placeholder="Last Name*" name="last_name"/><br />
   <input type="text" placeholder="Desired Username*" name="username"/><br />
   <input type="email" placeholder="Email*" name="email"/><br />
   <input type="password" placeholder="Enter a Password*" name="password"/><br />
