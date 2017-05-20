78960abc93fd6e22d6b6746908e8fe24caf6fb0e
John Cordero <bbbitemarksss@gmail.com>
2017-05-19 15:53:48 -0900

    Create test.js
    test

---
+++ src/js/test.js
@@ -0,0 +1,23 @@
+//this file will be a test for form validation via javascript.
+
+function checkAdd() {
+
+var title = $('#title').val();
+var message = $('#message').val();
+
+}
+
+function checkDelete() {
+
+
+}
+
+function checkAddUser() {
+
+}
+
+function checkRemoveUser() {
+
+
+}
+
